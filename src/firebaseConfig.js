efa87ddc3dc7652f3eb78571fe386e34ee53a837
Vijayalaxmi Shirnal <100484409+vijayalaxmishirnal01@users.noreply.github.com>
2026-04-06 10:54:18 +0530

    Set Firebase config with real API keys
    Updated Firebase configuration with actual credentials.

--- src/firebaseConfig.js
+++ src/firebaseConfig.js
@@ -6,13 +6,13 @@ import { getAnalytics } from "firebase/analytics";
 
 // Your Firebase config
 const firebaseConfig = {
-  apiKey: "",
-  authDomain: "",
-  projectId: "",
-  storageBucket: "",
-  messagingSenderId: "",
-  appId: "",
-  measurementId: ""
+  apiKey: "AIzaSyAchNwZV328nAei6hmkY7aVTsk4iQYu3Go",
+  authDomain: "crm-dashboard-a167f.firebaseapp.com",
+  projectId: "crm-dashboard-a167f",
+  storageBucket: "crm-dashboard-a167f.firebasestorage.app",
+  messagingSenderId: "385726277236",
+  appId: "1:385726277236:web:198004333612f058c25321",
+  measurementId: "G-10J92GNXPZ"
 };
 
 // Initialize Firebase
